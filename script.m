5878f9e04012e2271099741af8f9141b3bdf8c6a
Anuj Bhai Mehta <mehta.anuj2309@gmail.com>
2017-11-19 14:27:43 +0530

    added plotdata function

--- script.m
+++ script.m
@@ -1,4 +1,13 @@
 clear;
 
-//Load data
+% Load data
 data = load('data.txt');
+
+X = data(:, 1); 
+y = data(:, 2);
+
+% Number of Training Examples
+m = length(y);
+
+% Plot the data
+plotData(X, y);
